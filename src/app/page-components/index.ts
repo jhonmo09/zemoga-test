5c91f0c010fb1254b29163f3dc7ea428a5dc96f6
Jhon Moreno <jhonmoreno@Jhons-MacBook-Pro.local>
2024-02-11 17:55:58 -0500

    Basic structure

---
+++ src/app/page-components/index.ts
@@ -0,0 +1,5 @@
+export { default as Header } from './header/header';
+export { default as Navigation } from './navigation/navigation';
+export { default as BannerTop } from './banner-top/banner-top';
+export { default as BannerBottom } from './banner-bottom/banner-bottom';
+export { default as Footer } from './footer/footer';
